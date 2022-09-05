82ddb1d7e9a204be4c0f49d399c0340a7ca1462b
Rodrigo Barros <rodrigobarros2802@gmail.com>
2022-09-05 17:07:21 -0300

    Conclusão do Script

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,8 @@
 function mudaImagem(recebe) {
     document.querySelector('.starbucks').src = recebe;
 }
+
+function mudaCor(cor) {
+    const circle = document.querySelector('.circle');
+    circle.style.background = cor;
+}
